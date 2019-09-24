3c586732df3be6e63e4a60367bcba5e371e533b6
Danilo Tedeschi <danilof.t2@gmail.com>
2019-09-24 20:53:04 -0300

    wip

--- E3PNT/e3pnt.py
+++ E3PNT/e3pnt.py
@@ -6,15 +6,15 @@ from typing import List
 
 
 def _ccirc(x1, y1, x2, y2, x3, y3):
-        A = np.array([[x3 - x1, y3 - y1], [x3 - x2, y3 - y2]])
-        Y = np.array([(x3 ** 2 + y3 ** 2 - x1 ** 2 - y1 ** 2), (x3 ** 2 + y3 ** 2 - x2 ** 2 - y2 ** 2)])
-        if np.linalg.det(A) == 0:
-            return False
-        Ainv = np.linalg.inv(A)
-        X = 0.5 * np.dot(Ainv, Y)
-        x, y = X[0], X[1]
-        r = sqrt((x - x1) ** 2 + (y - y1) ** 2)
-        return (x, y), r
+    A = np.array([[x3 - x1, y3 - y1], [x3 - x2, y3 - y2]])
+    Y = np.array([(x3 ** 2 + y3 ** 2 - x1 ** 2 - y1 ** 2), (x3 ** 2 + y3 ** 2 - x2 ** 2 - y2 ** 2)])
+    if np.linalg.det(A) == 0:
+        return False
+    Ainv = np.linalg.inv(A)
+    X = 0.5 * np.dot(Ainv, Y)
+    x, y = X[0], X[1]
+    r = sqrt((x - x1) ** 2 + (y - y1) ** 2)
+    return (x, y), r
 
 
 def _tr(theta, q, x, y):
@@ -22,25 +22,25 @@ def _tr(theta, q, x, y):
 
 
 def _tri(theta, q, x, y):
-    x = x/q
-    return (x*cos(-theta) + y*sin(-theta)), -x * sin(-theta) + y * cos(-theta)
+    x = x / q
+    return (x * cos(-theta) + y * sin(-theta)), -x * sin(-theta) + y * cos(-theta)
 
 
 def _chk(theta, a, b, xc, yc, x, y):
-        X = x - xc
-        Y = y - yc
-        return (X * cos(theta) + Y * sin(theta)) ** 2 / a ** 2 + (
-                    X * sin(theta) - Y * cos(theta)) ** 2 / b ** 2 - 1e-9 < 1
+    X = x - xc
+    Y = y - yc
+    return (X * cos(theta) + Y * sin(theta)) ** 2 / a ** 2 + (
+            X * sin(theta) - Y * cos(theta)) ** 2 / b ** 2 - 1e-9 < 1
 
 
 def discard(a, b, X, Y):
     p = [np.array([X[i], Y[i]]) for i in range(3)]
 
-    return np.linalg.norm(p[0]-p[1]) > 2*a or np.linalg.norm(p[0]-p[2]) > 2*a or np.linalg.norm(p[1]-p[2]) > 2*a
+    return np.linalg.norm(p[0] - p[1]) > 2 * a or np.linalg.norm(p[0] - p[2]) > 2 * a or np.linalg.norm(
+        p[1] - p[2]) > 2 * a
 
 
 def get_input(X, Y):
-
     x0 = X[0]
     y0 = Y[0]
     x1 = X[1] - x0
@@ -51,7 +51,7 @@ def get_input(X, Y):
     return x0, y0, x1, y1, x2, y2
 
 
-def get_cheb_poly(a: float, b: float, X: List[float], Y:List[float]):
+def get_cheb_poly(a: float, b: float, X: List[float], Y: List[float]):
     XX = [X[i] for i in range(3)]
     YY = [Y[i] for i in range(3)]
 
@@ -64,27 +64,58 @@ def get_cheb_poly(a: float, b: float, X: List[float], Y:List[float]):
 
     for i in range(K):
         l = (i) * Dm
-        r = (i+1) * Dm
+        r = (i + 1) * Dm
         ret.append(np.polynomial.chebyshev.Chebyshev.interpolate(f, deg=deg, domain=(l, r), args=(a, b, XX, YY)))
 
     return ret
 
 
 def e3pnt(a: float, b: float, X: List[float], Y: List[float]):
+    if discard(a, b, X, Y):
+        return []
 
-    return []
+    x0 = X[0]
+    y0 = Y[0]
+
+    X[1] -= x0
+    Y[1] -= y0
+    X[2] -= x0
+    Y[2] -= y0
+
+    chebps = get_cheb_poly(a, b, X, Y)
+    angles = []
+
+    for chebpoly in chebps:
+        be = chebpoly.domain[0]
+        en = chebpoly.domain[1]
+
+        roo = list(filter(lambda t: np.isreal(t) and be <= t.real <= en, chebpoly.roots()))
+        angles.extend(np.real(roo))
+
+    ret = []
+
+    for theta in angles:
+        px1, py1 = _tr(theta, b / a, X[1], Y[1])
+        px2, py2 = _tr(theta, b / a, X[2], Y[2])
+        (xcc, ycc), r = _ccirc(0, 0, px1, py1, px2, py2)
+
+        xc, yc = _tri(theta, b / a, xcc, ycc)
+
+        if _chk(theta, a, b, xc, yc, X[1], Y[1]) and _chk(theta, a, b, xc, yc, X[2], Y[2]):
+            ret.append(((xc + x0, yc + y0), theta))
+
+    return ret
 
 
 def e3pnt_poly(a: float, b: float, X: List[float], Y: List[float]):
-
-    #if discard(a, b, X, Y):
+    # if discard(a, b, X, Y):
     #    return []
 
     x0, y0, x1, y1, x2, y2 = get_input(X, Y)
 
     coeff = poly_coeff(a, b, x1, x2, y1, y2)
     m = max(coeff)
-    coeff = list(map(lambda x: x/m, coeff))
+    coeff = list(map(lambda x: x / m, coeff))
 
     print(coeff)
 
@@ -95,10 +126,10 @@ def e3pnt_poly(a: float, b: float, X: List[float], Y: List[float]):
     print(min(abs(roo.imag)))
     print("-----")
 
-    #print(roo)
-    #print()
-    #print("--------------")
-    #print()
+    # print(roo)
+    # print()
+    # print("--------------")
+    # print()
 
     roots = list(filter(np.isreal, roo))
 
